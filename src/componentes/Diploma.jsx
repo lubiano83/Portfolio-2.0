865735af1050b792605fb65e530e4f6ae4703cc3
lubiano83 <lubiano83@gmail.com>
2024-03-17 23:41:53 -0300

    cambiando animaciones

--- src/componentes/Diploma.jsx
+++ src/componentes/Diploma.jsx
@@ -2,7 +2,7 @@
 
 const Diploma = ({img, titulo}) => {
   return (
-    <div id="Diploma" className="animate__animated animate__pulse">
+    <div id="Diploma" className="animate__animated animate__bounceIn">
         <img className="Diploma__foto" src={img} alt={titulo} />
     </div>
   )
