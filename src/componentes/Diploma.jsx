415ade034f4467f8078a2e6a8449be0d5f16034a
lubiano83 <lubiano83@gmail.com>
2024-03-18 09:28:16 -0300

    ajustando foto about y algunas animaciones

--- src/componentes/Diploma.jsx
+++ src/componentes/Diploma.jsx
@@ -2,7 +2,7 @@
 
 const Diploma = ({img, titulo}) => {
   return (
-    <div id="Diploma" className="animate__animated animate__bounceIn">
+    <div id="Diploma" className="animate__animated animate__fadeIn">
         <img className="Diploma__foto" src={img} alt={titulo} />
     </div>
   )
