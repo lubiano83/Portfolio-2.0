864c03c5f1ea7d0e7a526ee2ef2310adb536ae60
lubiano83 <lubiano83@gmail.com>
2024-04-14 17:37:09 -0400

    cambiando cv

--- src/screens/Name.jsx
+++ src/screens/Name.jsx
@@ -11,7 +11,7 @@ const Name = () => {
         <h1 className="display-4 animate__animated animate__backInRight" id="cargo">Aspiring Full-Stack Developer</h1>
         <div className="Name__container">
           <Titulo title="My CV:"/>
-          <a href="https://firebasestorage.googleapis.com/v0/b/portfolio-3e2be.appspot.com/o/CV-Jose%CC%81%20Pablo%20Lubiano%20conv.pdf?alt=media&token=192c9456-4401-4629-95f9-20eef0586b1c" target="_blank">
+          <a href="https://firebasestorage.googleapis.com/v0/b/portfolio-3e2be.appspot.com/o/CV-Jose%CC%81%20Pablo%20Lubiano%20conv.pdf?alt=media&token=ae8d8d1e-7b3a-49c9-ae02-edbc3159b93a" target="_blank">
             <Boton label="Link" />
           </a>
         </div>
