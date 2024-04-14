aaab79f218130d97181373b126b79d0eec8e16ac
lubiano83 <lubiano83@gmail.com>
2024-04-14 17:56:22 -0400

    cambaindo nombre opcion dropdown

--- src/componentes/Dropdown.jsx
+++ src/componentes/Dropdown.jsx
@@ -10,7 +10,7 @@ const Dropdown = () => {
         </button>
         <ul className="dropdown-menu">
             <NavLink to={"/"}><button className="dropdown-item" type="button">Name</button></NavLink>
-            <NavLink to={"/Courses"}><button className="dropdown-item" type="button">Courses</button></NavLink>
+            <NavLink to={"/Courses"}><button className="dropdown-item" type="button">Certificates</button></NavLink>
             <NavLink to={"/Tools"}><button className="dropdown-item" type="button">Tools</button></NavLink>
             <NavLink to={"/Works"}><button className="dropdown-item" type="button">Works</button></NavLink>
             <NavLink to={"/About"}><button className="dropdown-item" type="button">About</button></NavLink>
