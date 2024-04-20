0d2ecdfb4e23a84cca95f5094f175d7dc5f69d29
lubiano83 <lubiano83@gmail.com>
2024-04-20 12:41:23 -0400

    subiendo imagenes a firebase

--- src/screens/Tools.jsx
+++ src/screens/Tools.jsx
@@ -24,28 +24,28 @@ const Tools = () => {
           <article className="Tools__front-end animate__animated animate__bounceIn">
               <h2>Front-End:</h2>
               <div className="front-end__contenedor">
-                  <Foto img="src/svg/html-5-svgrepo-com.svg" label="html"/>
-                  <Foto img="src/svg/css-3-svgrepo-com.svg" label="css"/>
-                  <Foto img="src/svg/bootstrap-svgrepo-com.svg" label="bootstrap" />
-                  <Foto img="src/svg/sass-svgrepo-com.svg" label="sass"/>
-                  <Foto img="src/svg/javascript-svgrepo-com.svg" label="javascript"/>
-                  <Foto img="src/svg/react-svgrepo-com.svg" label="react"/>
-                  <Foto img="src/svg/redux-svgrepo-com (2).svg" label="redux"/>
+                  <Foto img="https://firebasestorage.googleapis.com/v0/b/portfolio-3e2be.appspot.com/o/img%2Fhtml-5-svgrepo-com.svg?alt=media&token=1b29d3a6-b8f2-4b2a-abb0-b63ca37d2008" label="html"/>
+                  <Foto img="https://firebasestorage.googleapis.com/v0/b/portfolio-3e2be.appspot.com/o/img%2Fcss-3-svgrepo-com.svg?alt=media&token=8dfa4fb6-ddf9-4c3e-ad08-aab61205f2fe" label="css"/>
+                  <Foto img="https://firebasestorage.googleapis.com/v0/b/portfolio-3e2be.appspot.com/o/img%2Fbootstrap-svgrepo-com.svg?alt=media&token=a7b5f665-bc60-4df2-998f-b915e2114968" label="bootstrap" />
+                  <Foto img="https://firebasestorage.googleapis.com/v0/b/portfolio-3e2be.appspot.com/o/img%2Fsass-svgrepo-com.svg?alt=media&token=beac4649-02ac-4d72-b677-5da54f725efc" label="sass"/>
+                  <Foto img="https://firebasestorage.googleapis.com/v0/b/portfolio-3e2be.appspot.com/o/img%2Fjavascript-svgrepo-com.svg?alt=media&token=7eea10c9-fb07-4220-bf7c-b514913e9acd" label="javascript"/>
+                  <Foto img="https://firebasestorage.googleapis.com/v0/b/portfolio-3e2be.appspot.com/o/img%2Freact-svgrepo-com.svg?alt=media&token=58a8ce64-09cb-4d13-b16e-ea74ac932c27" label="react"/>
+                  <Foto img="https://firebasestorage.googleapis.com/v0/b/portfolio-3e2be.appspot.com/o/img%2Fredux-svgrepo-com%20(2).svg?alt=media&token=aa9529d8-ea28-4c02-8d46-37878aed3297" label="redux"/>
               </div>
           </article> }
           {mostrar1 && 
           <article className="Tools__back-end animate__animated animate__bounceIn">
             <h2>Back-End:</h2>
             <div className="back-end__contenedor">
-                <Foto id="firebase" img="src/svg/firebase-svgrepo-com.svg" label="firebase"/>
+                <Foto id="firebase" img="https://firebasestorage.googleapis.com/v0/b/portfolio-3e2be.appspot.com/o/img%2Ffirebase-svgrepo-com.svg?alt=media&token=ffe90477-653b-4046-9277-d05bf3c91901" label="firebase"/>
             </div>
           </article> }
           { mostrar2 &&
           <article className="Tools__tools animate__animated animate__bounceIn">
             <h2>Tools:</h2>
             <div className="tools__contenedor">
-              <Foto img="src/svg/github-icon-svgrepo-com.svg" label="github"/>
-              <Foto img="src/svg/filezilla-ftp-software-svgrepo-com.svg" label="filezilla"/>
+              <Foto img="https://firebasestorage.googleapis.com/v0/b/portfolio-3e2be.appspot.com/o/img%2Fgithub-icon-svgrepo-com.svg?alt=media&token=825ad433-cee0-4d6c-94b3-ca0500bd31b2" label="github"/>
+              <Foto img="https://firebasestorage.googleapis.com/v0/b/portfolio-3e2be.appspot.com/o/img%2Ffilezilla-ftp-software-svgrepo-com.svg?alt=media&token=3eeadd5b-cb2e-45bb-8418-6198574b0d57" label="filezilla"/>
           </div> 
           </article>}
         </div>
