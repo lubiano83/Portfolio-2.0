75b8251c527498b6d16e570d721af8f4506384ed
lubiano83 <lubiano83@gmail.com>
2024-04-22 12:43:40 -0400

    ajustando about

--- src/screens/About.jsx
+++ src/screens/About.jsx
@@ -6,13 +6,15 @@ const About = () => {
   return (
     <section id="About">
         <Titulo title="About Me:"/>
-        <div className="About__img">
-          <img src="https://firebasestorage.googleapis.com/v0/b/portfolio-3e2be.appspot.com/o/img%2Flugar-trabajo.webp?alt=media&token=83e02363-ea61-4f60-905f-c75413372b75" alt="oficina" className="animate__animated animate__bounceInDown"/>
-        </div>
-        <div className="About__texto">
-            <h5 className="animate__animated animate__bounceInLeft">After working for 10 years in the position of Branch Manager in an automotive company in Viña del Mar, Chile, achieving excellent results, I decided to change direction in my professional career.</h5>
-            <h5 className="animate__animated animate__bounceInRight">Today I am studying a full stack web development career, which will not end there, but will continue specializing in the different tools that I consider essential.</h5>
-            <h5 className="animate__animated animate__bounceInUp">In the short term, I intend to dedicate 100% of my time to web development and be able to make a difference with the rest of the developers, complementing it with my work experience and my business approach.</h5>
+        <div className="About__container">
+          <div className="About__img">
+            <img src="https://firebasestorage.googleapis.com/v0/b/portfolio-3e2be.appspot.com/o/img%2Flugar-trabajo.webp?alt=media&token=83e02363-ea61-4f60-905f-c75413372b75" alt="oficina" className="animate__animated animate__bounceInLeft"/>
+          </div>
+          <div className="About__texto">
+              <h5 className="animate__animated animate__bounceInRight">After working for 10 years in the position of Branch Manager in an automotive company in Viña del Mar, Chile, achieving excellent results, I decided to change direction in my professional career.</h5>
+              <h5 className="animate__animated animate__bounceInRight">Today I am studying a full stack web development career, which will not end there, but will continue specializing in the different tools that I consider essential.</h5>
+              <h5 className="animate__animated animate__bounceInRight">In the short term, I intend to dedicate 100% of my time to web development and be able to make a difference with the rest of the developers, complementing it with my work experience and my business approach.</h5>
+          </div>
         </div>
     </section>
   )
