02e24909d4f83b7bc6d77eb05f324e9d75d296fa
lubiano83 <lubiano83@gmail.com>
2024-04-28 17:50:45 -0400

    ajuste pequeño contacto

--- src/screens/Contact.jsx
+++ src/screens/Contact.jsx
@@ -5,7 +5,7 @@ import Titulo from "../componentes/Titulo";
 
 const Contact = () => {
   return (
-    <form id="Contact" action="https://formsubmit.co/lubiano83@gmail.com" method="POST">
+    <form id="Contact" action="https://formsubmit.co/lubiano83@gmail.com" method="POST" target="_blank">
         <Titulo title="Contact Me:"/>
         <input type="email" name="email" className="form-control input" id="exampleFormControlInput1" placeholder="Coloca tu Email" required />
         <textarea className="form-control textarea" name="textarea" type="textarea" id="exampleFormControlTextarea1" rows="3" placeholder="Deja tu Mensaje" required />
