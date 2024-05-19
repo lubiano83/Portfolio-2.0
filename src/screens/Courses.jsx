01173d8352db20ff5b12ee71be0066cd7e1b030e
lubiano83 <lubiano83@gmail.com>
2024-05-19 16:15:55 -0400

    agregando diploma desarrollo de aplicaciones

--- src/screens/Courses.jsx
+++ src/screens/Courses.jsx
@@ -11,6 +11,7 @@ const Courses = () => {
         <Diploma img="https://firebasestorage.googleapis.com/v0/b/portfolio-3e2be.appspot.com/o/img%2FDesarrollo-Web.png?alt=media&token=d99e91e5-fc34-4281-b9fc-cc9ebe3d7f40" titulo="Desarrollo-Web"/>
         <Diploma img="https://firebasestorage.googleapis.com/v0/b/portfolio-3e2be.appspot.com/o/img%2FJavaScript.png?alt=media&token=28edb74b-2a6d-42ee-9cfd-88601bb604bc" titulo="JavaScript"/>
         <Diploma img="https://firebasestorage.googleapis.com/v0/b/portfolio-3e2be.appspot.com/o/img%2FDiploma%20React%20Js.png?alt=media&token=d3b78ca1-1680-46ce-b60f-d0b644aaee94" titulo="React Js"/>
+        <Diploma img="https://firebasestorage.googleapis.com/v0/b/portfolio-3e2be.appspot.com/o/img%2FDesarrollo%20de%20Aplicaciones.webp?alt=media&token=43945756-2fa2-4492-bf19-891ec6a6178d" titulo="Desarrollo de Aplicaciones"/>
       </div>
     </div>
   )
