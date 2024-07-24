0f53d55ec0a0e5a0118346a23c6812fd6e0b7496
lubiano83 <lubiano83@gmail.com>
2024-07-23 20:50:35 -0400

    agregando diploma backend y tools

--- src/screens/Courses.jsx
+++ src/screens/Courses.jsx
@@ -12,6 +12,7 @@ const Courses = () => {
         <Diploma img="https://firebasestorage.googleapis.com/v0/b/portfolio-3e2be.appspot.com/o/img%2FJavaScript.png?alt=media&token=28edb74b-2a6d-42ee-9cfd-88601bb604bc" titulo="JavaScript"/>
         <Diploma img="https://firebasestorage.googleapis.com/v0/b/portfolio-3e2be.appspot.com/o/img%2FDiploma%20React%20Js.png?alt=media&token=d3b78ca1-1680-46ce-b60f-d0b644aaee94" titulo="React Js"/>
         <Diploma img="https://firebasestorage.googleapis.com/v0/b/portfolio-3e2be.appspot.com/o/img%2FDesarrollo%20de%20Aplicaciones.webp?alt=media&token=43945756-2fa2-4492-bf19-891ec6a6178d" titulo="Desarrollo de Aplicaciones"/>
+        <Diploma img="https://firebasestorage.googleapis.com/v0/b/portfolio-3e2be.appspot.com/o/img%2FBackEnd-1.webp?alt=media&token=0099d3c2-fc0c-4985-a866-d44a0891a0f3" titulo="Programacion BackEnd 1: Desarrollo Avanzado de BackEnd"/>
       </div>
     </div>
   )
--- src/screens/Tools.jsx
+++ src/screens/Tools.jsx
@@ -29,8 +29,11 @@ const Tools = () => {
                   <Foto img="https://firebasestorage.googleapis.com/v0/b/portfolio-3e2be.appspot.com/o/img%2Fbootstrap-svgrepo-com.svg?alt=media&token=a7b5f665-bc60-4df2-998f-b915e2114968" label="bootstrap" />
                   <Foto img="https://firebasestorage.googleapis.com/v0/b/portfolio-3e2be.appspot.com/o/img%2Fsass-svgrepo-com.svg?alt=media&token=beac4649-02ac-4d72-b677-5da54f725efc" label="sass"/>
                   <Foto img="https://firebasestorage.googleapis.com/v0/b/portfolio-3e2be.appspot.com/o/img%2Fjavascript-svgrepo-com.svg?alt=media&token=7eea10c9-fb07-4220-bf7c-b514913e9acd" label="javascript"/>
-                  <Foto img="https://firebasestorage.googleapis.com/v0/b/portfolio-3e2be.appspot.com/o/img%2Freact-svgrepo-com.svg?alt=media&token=58a8ce64-09cb-4d13-b16e-ea74ac932c27" label="react"/>
+                  <Foto img="https://firebasestorage.googleapis.com/v0/b/portfolio-3e2be.appspot.com/o/img%2Freact-js.svg?alt=media&token=47f2d7fd-80da-4ac7-908a-b0fcff5305de" label="react"/>
+                  <Foto img="https://firebasestorage.googleapis.com/v0/b/portfolio-3e2be.appspot.com/o/img%2Freact-native.svg?alt=media&token=05210d6c-683c-41c1-9974-25873b03d9bd" label="reactnative"/>
                   <Foto img="https://firebasestorage.googleapis.com/v0/b/portfolio-3e2be.appspot.com/o/img%2Fredux-svgrepo-com%20(2).svg?alt=media&token=aa9529d8-ea28-4c02-8d46-37878aed3297" label="redux"/>
+                  <Foto img="https://firebasestorage.googleapis.com/v0/b/portfolio-3e2be.appspot.com/o/img%2Fnext-js-svgrepo-com.svg?alt=media&token=11e44535-8686-40f8-8c2b-dc46a54d111d" label="nextjs"/>
+                  <Foto img="https://firebasestorage.googleapis.com/v0/b/portfolio-3e2be.appspot.com/o/img%2Ftailwind-svgrepo-com.svg?alt=media&token=c1d4f5ea-4e98-4a42-9c32-159e4c475c4c" label="tailwind"/>
               </div>
           </article> }
           {mostrar1 && 
@@ -40,6 +43,7 @@ const Tools = () => {
                 <Foto id="firebase" img="https://firebasestorage.googleapis.com/v0/b/portfolio-3e2be.appspot.com/o/img%2Ffirebase-svgrepo-com.svg?alt=media&token=ffe90477-653b-4046-9277-d05bf3c91901" label="firebase"/>
                 <Foto id="nodeJs" img="https://firebasestorage.googleapis.com/v0/b/portfolio-3e2be.appspot.com/o/img%2Fnode-js-svgrepo-com.svg?alt=media&token=5066c33a-41a9-40e0-93c9-0ece291dc0a3" label="nodeJs"/>
                 <Foto id="express" img="https://firebasestorage.googleapis.com/v0/b/portfolio-3e2be.appspot.com/o/img%2Fexpress-svgrepo-com.svg?alt=media&token=974a9cf8-2f27-463d-8a30-5cd1382c2f61" label="express"/>
+                <Foto id="mongodb" img="https://firebasestorage.googleapis.com/v0/b/portfolio-3e2be.appspot.com/o/img%2Fmongo-svgrepo-com.svg?alt=media&token=1a654408-d6b8-41d9-8fba-88d5604a4be4" label="mongodb"/>
             </div>
           </article> }
           { mostrar2 &&
